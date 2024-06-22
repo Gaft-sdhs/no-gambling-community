856cf9519698fb8ae6b2adaa413ac260db30a517
darelljay <darelljason1@gmail.com>
2024-06-22 22:03:06 +0900

    initial commit

--- community/src/App.tsx
+++ community/src/App.tsx
@@ -1,20 +1,20 @@
 import React from 'react';
-import Header from './components/Header';
-import MainContent from './components/MainContent';
-import Footer from './components/Footer';
-import styled from 'styled-components';
+// import Header from './components/Header';
+// import MainContent from './components/MainContent';
+// import Footer from './components/Footer';
+import styled from 'styled-components';                                                         
 
 const Container = styled.div`
   width: 90%;
   margin: auto;
 `;
 
-const App = () => {
+const App = () => { 
   return (
     <Container>
-      <Header />
-      <MainContent />
-      <Footer />
+      {/* <Header /> */}
+      {/* <MainContent /> */}
+      {/* <Footer /> */}
     </Container>
   );
 };
---
+++ community/src/components/Header/HeaderStyle.ts
@@ -0,0 +1,22 @@
+import styled, { Styled } from "styled-components";
+
+
+const HeaderBox:Styled = styled.header`
+    width: 100%;
+    height: 95px;
+    background-color: #2B3142;
+    color: #fff;
+`;
+
+const ImgWrap:Styled = styled.div`
+    width: 20%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+const FirstHalf:Styled = styled.div`
+    width: 50%;
+    height: 100%;
+`;
